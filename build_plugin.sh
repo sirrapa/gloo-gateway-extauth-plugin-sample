75d7a6840a4a0f4f5e645ec188a666fa3c5a2e1b
Armand Parris <armand.parris@sirrapa.com>
2020-04-29 16:47:09 +0200

    reuse env var

--- build_plugin.sh
+++ build_plugin.sh
@@ -16,7 +16,7 @@ plugin_output_path=_output
 
 # Build image which holds the plugin framework and custom implementation
 docker build --no-cache -f Dockerfile \
-    --build-arg ENV_GLOOE_VERSION=1.3.0 \
+    --build-arg ENV_GLOOE_VERSION=$GLOOE_VERSION \
     --build-arg ENV_STORAGE_HOSTNAME=$STORAGE_HOSTNAME \
     --build-arg GO_BUILD_IMAGE=$GO_BUILD_IMAGE \
     --build-arg PLUGIN_FRAMEWORK_VERSION=$plugin_framework_version \
